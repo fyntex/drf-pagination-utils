c4d0dd34d166ddfdcfb49e58ed2a987c44e9fe20
Yasel Couce <ycouce@fyntex.ai>
2021-10-16 01:02:45 -0300

    drf_pagination_utils: Add Link Header Page Number pagination style
    A simple page number–based style that supports page numbers as query
    parameters and includes pagination links in an RFC 8288–compliant `Link`
    header.
    
    Example URLs:
    - http://www.example.com/example-items/?page=4
    - http://www.example.com/example-items/?page=4&page_size=100
    
    Example header:
    
    ```
    Link: <http://www.example.com/example-items/>; rel="first",
     <http://www.example.com/example-items/?page=3>; rel="previous",
     <http://www.example.com/example-items/?page=5>; rel="next",
     <http://www.example.com/example-items/?page=42>; rel="last"
    ```
    
    See also:
    - https://tools.ietf.org/html/rfc8288
    - https://requests.readthedocs.io/en/latest/user/advanced/#link-headers
    - https://docs.github.com/en/rest/guides/traversing-with-pagination
    
    -----
    
    Sourced as is from:
    - https://github.com/fyntex/fd-cl-data/blob/2f9e75e34a400fe039094b96d70584930f6cd7c0/fd_cl_data/libs/drf_pagination_utils.py
    - https://github.com/fyntex/fd-cl-data/blob/2f9e75e34a400fe039094b96d70584930f6cd7c0/fd_cl_data/libs/tests/test_drf_pagination_utils.py
    
    All the credit for these changes goes to jtrobles@cordada.com
    
    Co-authored-by: José Tomás Robles Hahn <jtrobles@cordada.com>
    Ref: https://cordada.aha.io/features/PLATCORE-266
    Ref: https://cordada.aha.io/requirements/PLATCORE-266-2

---
+++ fyntex/drf_pagination_utils/styles.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+from typing import Iterable, Mapping, Optional
+
+import rest_framework.pagination
+from rest_framework.response import Response as DrfResponse
+from rest_framework.utils.urls import remove_query_param, replace_query_param
+
+
+class LinkHeaderPageNumberPagination(rest_framework.pagination.PageNumberPagination):
+    """
+    A simple page number–based style that supports page numbers as query parameters and includes
+    pagination links in an RFC 8288–compliant `Link` header.
+
+    Example URLs:
+    - http://www.example.com/example-items/?page=4
+    - http://www.example.com/example-items/?page=4&page_size=100
+
+    Example header:
+    Link: <http://www.example.com/example-items/>; rel="first",
+     <http://www.example.com/example-items/?page=3>; rel="previous",
+     <http://www.example.com/example-items/?page=5>; rel="next",
+     <http://www.example.com/example-items/?page=42>; rel="last"
+
+    See also:
+    - https://tools.ietf.org/html/rfc8288
+    - https://requests.readthedocs.io/en/latest/user/advanced/#link-headers
+    - https://docs.github.com/en/rest/guides/traversing-with-pagination
+    """
+
+    _LINK_VALUE_TEMPLATE: str = '<{uri_reference}>; rel="{relation_type}"'
+    """
+    Link value template string.
+
+    See also: [Link Serialisation in HTTP Headers](https://tools.ietf.org/html/rfc8288#section-3)
+    """
+
+    def get_paginated_response(self, data: object) -> DrfResponse:
+        links: Mapping[str, Optional[str]] = {
+            'first': self.get_first_link(),
+            'previous': self.get_previous_link(),
+            'next': self.get_next_link(),
+            'last': self.get_last_link(),
+        }
+        link_values: Iterable[str] = (
+            self._LINK_VALUE_TEMPLATE.format(uri_reference=uri_ref, relation_type=rel_type)
+            for rel_type, uri_ref in links.items()
+            if uri_ref is not None
+        )
+
+        headers: Optional[Mapping[str, str]] = None
+        if link_values:
+            headers = {
+                'Link': ', '.join(link_values),
+            }
+
+        return DrfResponse(data, headers=headers)
+
+    def get_last_link(self) -> Optional[str]:
+        if not self.page.has_next():
+            return None
+        url = self.request.build_absolute_uri()
+        page_number = self.page.paginator.num_pages
+        result_url: str = replace_query_param(url, self.page_query_param, page_number)
+        return result_url
+
+    def get_first_link(self) -> Optional[str]:
+        if not self.page.has_previous():
+            return None
+        url = self.request.build_absolute_uri()
+        result_url: str = remove_query_param(url, self.page_query_param)
+        return result_url
